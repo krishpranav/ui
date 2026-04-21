4115ffcb7f5249c9757e62973deae842f0eaf860
Max Wells <maxwells.pro@proton.me>
2026-04-21 10:17:24 +0700

    fix: DemoSkeletonText -> w-full

--- app_crates/registry/src/demos/demo_skeleton_text.rs
+++ app_crates/registry/src/demos/demo_skeleton_text.rs
@@ -5,7 +5,7 @@ use crate::ui::skeleton::Skeleton;
 #[component]
 pub fn DemoSkeletonText() -> impl IntoView {
     view! {
-        <div class="flex flex-col gap-2 max-w-sm">
+        <div class="flex flex-col gap-2 w-full max-w-sm">
             <Skeleton class="w-full h-4" />
             <Skeleton class="h-4 w-[90%]" />
             <Skeleton class="w-full h-4" />
